70a93109b9ede092ab979a97ce4db854f309cdfe
dsanimm <dsanimm@gmail.com>
2020-10-05 09:06:20 +0530

    CreatedBoard

---
+++ src/Workshop/TicTacToe.java
@@ -0,0 +1,12 @@
+package Workshop;
+public class TicTacToe {
+	
+	public TicTacToe(){
+		char board[] = new char[11];
+		for(int i=1;i<11;i++)
+				board[i]=' ';
+	}
+public static void main(String[] args) {
+	TicTacToe obj = new TicTacToe();
+}
+}
