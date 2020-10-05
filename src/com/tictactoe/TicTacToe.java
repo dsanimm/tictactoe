731f8d6964038f21e87561da58313877e1b3c0af
dsanimm <dsanimm@gmail.com>
2020-10-05 11:21:36 +0530

    UserPosition

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -3,7 +3,7 @@ package com.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-	static Scanner sc = new Scanner(System.in);
+	private static final Scanner sc = new Scanner(System.in);
 
 	private static char[] userChoice(char[] board) {
 		System.out.println("Choose X or O");
@@ -18,8 +18,8 @@ public class TicTacToe {
 	}
 
 	private static char[] createBoard() {
-		char board[] = new char[11];
-		for (int i = 1; i < 11; i++)
+		char board[] = new char[10];
+		for (int i = 1; i < board.length; i++)
 			board[i] = ' ';
 		return board;
 	}
@@ -27,7 +27,7 @@ public class TicTacToe {
 	private static char[] choiceBoard(char[] board, char choice, int pos) {
 		board[pos] = choice;
 		int posComputer = (int) (10 * (Math.random())) + 1;
-		while (posComputer != ' ') {
+		while (board[posComputer] != ' ') {
 			posComputer = (int) (10 * (Math.random())) + 1;
 		}
 		char compLetter = (choice == 'O') ? 'X' : 'O';
@@ -38,7 +38,7 @@ public class TicTacToe {
 	}
 
 	private static void printBoard(char[] board) {
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < board.length; i++) {
 			System.out.print("[" + board[i] + "]");
 			if (i % 3 == 0) {
 				System.out.println();
@@ -47,14 +47,43 @@ public class TicTacToe {
 		}
 	}
 
+	/*
+	 * private static boolean checkDraw(char[] board) { boolean flag = false; for
+	 * (int i = 1; i < board.length; i++) { if (board[i] == ' ') ; flag = true; }
+	 * return flag; }
+	 */
+
+	/*
+	 * private static boolean checkWin(char[] board) { for (int a = 0; a < 8; a++) {
+	 * StringBuilder line = null; switch (a) { case 0: line = new
+	 * StringBuilder().append(board[1]).append(board[2]).append(board[3]); break;
+	 * case 1: line = new
+	 * StringBuilder().append(board[4]).append(board[5]).append(board[6]); break;
+	 * case 2: line = new
+	 * StringBuilder().append(board[7]).append(board[8]).append(board[9]); break;
+	 * case 3: line = new
+	 * StringBuilder().append(board[1]).append(board[4]).append(board[7]); break;
+	 * case 4: line = new
+	 * StringBuilder().append(board[2]).append(board[5]).append(board[8]); break;
+	 * case 5: line = new
+	 * StringBuilder().append(board[3]).append(board[6]).append(board[9]); break;
+	 * case 6: line = new
+	 * StringBuilder().append(board[1]).append(board[5]).append(board[9]); break;
+	 * case 7: line = new
+	 * StringBuilder().append(board[3]).append(board[5]).append(board[7]); break; }
+	 * if (line.equals("XXX") || line.equals("OOO")) { return true; } } return
+	 * false; }
+	 */
+
 	public static void main(String[] args) {
 
-		TicTacToe obj = new TicTacToe();
-		TicTacToe.createBoard();
+		createBoard();
 		char[] board = TicTacToe.createBoard();
-		TicTacToe.printBoard(board);
-		board = TicTacToe.userChoice(board);
-		TicTacToe.printBoard(board);
+		printBoard(board);
+		//while (checkWin(board)||checkDraw(board)) {
+			board = TicTacToe.userChoice(board);
+			printBoard(board);
+		}
 
 	}
 
